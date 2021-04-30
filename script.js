28e38fc6714e876fb3284292b8d7c9274cd05794
laureCh81 <71818016+laureCh81@users.noreply.github.com>
2021-04-30 15:32:57 +0200

    30/04 15h32

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+// JQUERY
 $(function(){
 
 if(window.innerWidth <= 480) {
-        $("#header").removeClass("fixed-top");
-       
-    }
-
-
+        $("#header").removeClass("fixed-top");   
+       }
 });
+
+//---- JS
+//Check Forms
+  
+ let mailForm = document.querySelector('.mailForm');
+ mailForm.addEventListener('blur', (e)=>{
+    let validationMail = '';
+    let regexMail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;  
+    if(!regexMail.test(e.target.value)){
+        validationMail = 'Veuillez renseigner une adresse valide';
+    }
+    document.querySelector('.mailFalse').textContent = validationMail;
+    });
+
+    let surname = document.querySelector('.surname');  
+    surname.addEventListener('blur', (e)=>{     
+    let prenom = e.target.value;
+    let validationPrenom = '';
+    if(prenom.length < 1){
+        validationPrenom = 'Indiquez votre prenom';
+    }
+    document.querySelector('.prenomFalse').textContent = validationPrenom;
+});
+
+    let nom = document.querySelector('.nom');
+    nom.addEventListener('blur', (e)=>{     
+        let valName = e.target.value;
+        let validationName = '';
+        if(valName.length < 1){
+            validationName = 'Indiquez votre nom';
+        }
+        document.querySelector('.nameFalse').textContent = validationName;
+    });
+
+    let closeWin = document.querySelector('.closeWin');
+    closeWin.addEventListener('click', (e)=>{     
+        surname.value='';
+        nom.value='';
+        mailForm.value='';
+    });
+
+    
